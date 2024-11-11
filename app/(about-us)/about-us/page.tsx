f48b82b8166389dabd0f3a9b9b2e2039c48c3fdb
TechInnovatorAI <masatotanaka0219@gmail.com>
2024-11-11 13:51:15 -0500

    make: about, partner, faq

---
+++ app/(about-us)/about-us/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/landing/about/Hero"
+import Hero2 from "@/components/landing/about/Hero2"
+import Hero3 from "@/components/landing/about/Hero3"
+import Hero4 from "@/components/landing/about/Hero4";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Hero2 />
+      <Hero3 />
+      <Hero4 />
+    </>
+  );
+}
--- components/landing/faqs/Hero3.tsx
+++ components/landing/faqs/Hero3.tsx
@@ -10,11 +10,11 @@ const Form8 = () => {
                 <div className="relative items-center py-16 justify-center h-full">
                     <Image src={LeftImage} alt="Logo" className="absolute inset-0 -z-10 w-full h-[150%]" style={{ objectFit: 'cover' }} />
                     <div className="text-left max-w-4xl mx-auto px-4 py-14">
-                        <h1 className="text-white px-[15%] text-8xl md:text-4xl mb-6 font-clash-display ">
-                            For More Info Contact Us Today
+                        <h1 className="text-white px-[15%] text-8xl md:text-4xl mb-6 ">
+                        Still have Questions?
                         </h1>
-                        <h2 className="text-white px-[15%] text-xl md:text-md font-semibold  font-clash-display ">
-                            Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt labore.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        <h2 className="text-gray-300 px-[15%] text-xl md:text-md ">
+                        If you haven’t found the answer you’re looking for in our FAQs, we’re here to help! Reach out to our support team, and we’ll assist you with any questions or issues you may have. Contact us now for personalized support.
                         </h2>
                     </div>
                 </div>
@@ -49,7 +49,7 @@ const Form8 = () => {
                 </div>
                 <div className="layer4 mb-4 px-4">
                     <label className="block text-white">Message or Query</label>
-                    <textarea placeholder="Type you Message or Query Herer........" className="w-full p-2 rounded-2xl border border-gray-400 bg-[#004F8F]" />
+                    <textarea placeholder="Type you Message or Query Here......." className="w-full p-2 rounded-2xl border border-gray-400 bg-[#004F8F]" />
                 </div>
                 <div className="layer5 px-4">
                     <button className="bg-white text-[#004F8F] p-2 w-1/3 rounded-lg">Send Message</button>
