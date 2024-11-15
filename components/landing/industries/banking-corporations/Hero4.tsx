c794e287764040fb0a21b25ad8c1a6bdb70a1bf9
InnovatorAI618 <condolvhann618@gmail.com>
2024-11-15 12:20:39 -0500

    make: banking, focused lender

--- components/landing/industries/banking-corporations/Hero4.tsx
+++ components/landing/industries/banking-corporations/Hero4.tsx
@@ -10,7 +10,7 @@ const Hero4 = () => {
             <div className="px-[10%] py-16 flex flex-row justify-center items-center rounded-lg ">
                 <div className="flex-1  flex flex-col justify-center items-start p-16">
                     <h1 className="text-md text-gray-300 font-bold mb-4">Lorem Ipsum dolor</h1>
-                    <h2 className="text-4xl font-bold mb-4">Community Credit Groups, <span className="text-[#004F8F]">Prepared to Succeed?</span></h2>
+                    <h2 className="text-4xl font-bold mb-4">Bankers, are you <span className="text-[#004F8F]">Prepared to Succeed?</span></h2>
                     <p className="text-md font-light mb-4">Request a no-obligation, custom proof of concept to see the real benefits and opportunities for your lending organization and members.</p>
                     <button className="bg-[#004F8F] text-white px-6 py-3 rounded-2xl font-bold">
                         Lets connect
