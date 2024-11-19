365ec7dc771dcde79c47b28f6605d9dea414b335
InnovatorAI618 <condolvhann618@gmail.com>
2024-11-19 15:37:50 -0500

    fix: top-back

--- components/landing/industries/banking-corporations/Hero.tsx
+++ components/landing/industries/banking-corporations/Hero.tsx
@@ -1,31 +1,16 @@
 "use client";
-import Link from "next/link";
-import Image from "next/image"
+import TopHero from "@/components/ui/top-hero";
 
 const Hero = () => {
     return (
-        <>
-            <div className="mt-24"></div>
-            <div className="relative bg-[#004F8F] min-h-screen flex items-center justify-center">
-                {/* <div className="absolute -z-10 w-full ">{TopImage}</div> */}
-                <div className="text-center max-w-4xl mx-auto px-4 py-16">
-                    <h1 className="text-white text-2xl md:text-3xl mb-6 font-clash-display uppercase">
-                    Risk Evaluation
-                    </h1>
-
-                    <h2 className="text-white text-5xl md:text-6xl font-semibold mb-4 font-clash-display ">
-                    Intelligent, Inclusive, and Efficient AI-Driven Risk Evaluation
-                    </h2>
-
-                    <p className="text-white text-lg mb-12  max-w-2xl mx-auto font-clash-display">
-                    Deep lending insights for accurate borrower assessments.
-                    </p>
-                    <button className="bg-white text-blue-500 py-2 px-4 rounded-full hover:bg-blue-500 hover:text-white transition duration-300">
-                    Request a proof of Concept
-                    </button>
-                </div>
-            </div>
-        </>
+        <div className="p-4">
+            <TopHero
+                subtitle="Risk Evaluation"
+                title="Intelligent, Inclusive, and Efficient AI-Driven Risk Evaluation"
+                description="Deep lending insights for accurate borrower assessments."
+                button="Request a proof of Concept"
+            />
+        </div>
     );
 };
 
--- components/landing/solutions/lending-analytics/Hero.tsx
+++ components/landing/solutions/lending-analytics/Hero.tsx
@@ -1,30 +1,16 @@
 "use client";
+import TopHero from "@/components/ui/top-hero";
 
 const Hero = () => {
     return (
-        <>
-            <div className="mt-24"></div>
-            <div className="relative bg-[#004F8F] min-h-screen flex items-center justify-center">
-                {/* <div className="absolute -z-10 w-full ">{TopImage}</div> */}
-                <div className="foreground"></div>
-                <div className="text-center max-w-4xl mx-auto px-4 py-16">
-                    <h1 className="text-white uppercase text-2xl md:text-3xl mb-6 font-clash-display ">
-                    Lending Analytics
-                    </h1>
-
-                    <h2 className="text-white text-5xl md:text-6xl font-semibold mb-4 font-clash-display ">
-                    Enhanced visibility and flexibility with Lending Analytics
-                    </h2>
-
-                    <p className="text-white text-lg mb-12  max-w-2xl mx-auto font-clash-display">
-                    Refine your lending strategy with actionable insights and reliable accuracy
-                    </p>
-                    <button className="bg-white text-blue-500 py-2 px-4 rounded-full hover:bg-blue-500 hover:text-white transition duration-300">
-                    Request a Proof of Concept
-                    </button>
-                </div>
-            </div>
-        </>
+        <div className="p-4">
+            <TopHero
+                subtitle="Lending Analytics"
+                title="Enhanced visibility and flexibility with Lending Analytics"
+                description="Refine your lending strategy with actionable insights and reliable accuracy"
+                button="Request a Proof of Concept"
+            />
+        </div>
     );
 };
 
