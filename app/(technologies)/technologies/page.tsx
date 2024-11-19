3e51803b0eb0681e670bedd460a0575122acbf29
TechInnovatorAI <masatotanaka0219@gmail.com>
2024-11-19 15:23:15 -0500

    base-final

---
+++ app/(technologies)/technologies/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/landing/technologies/Hero"
+import Hero2 from "@/components/landing/technologies/Hero2"
+
+export default function Technologies() {
+  return (
+    <>
+      <Hero />
+      <Hero2 />
+    </>
+  );
+}
--- components/landing/blog/Hero2.tsx
+++ components/landing/blog/Hero2.tsx
@@ -1,77 +1,122 @@
+"use client"
 import React from 'react';
+import {useState} from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
+import Blog from '@/components/ui/blog-card'
+
+const CategoryList = [
+    "All Blogs & Articles", "Compilance", "Credit and Risk", "Culture", "Industry", "Technology", "AiBuildIQ"
+]
 
 const BlogList = [
     {
-        image: "",
-        category: [],
-        title: "",
-        description: "",
+        id: 1,
+        image: "/assets/how-it-works/1.jpg",
+        category: [1,3],
+        title: "Learning from yesterday’s credit union movement for a better tomorrow",
+        description: "Every year, we celebrate International Credit Union (ICU) Day on the third Thursday of October.",
+        link: ""
     },
     {
-        image: "",
-        category: [],
-        title: "",
-        description: "",
+        id: 2,
+        image: "/assets/how-it-works/1.jpg",
+        category: [1,3],
+        title: "Alternative data is just one consideration for a lender’s credit data strategy",
+        description: "Many fintechs are promoting this add-on as a panacea for any trouble related to model.",
+        link: ""
     },
     {
-        image: "",
-        category: [],
-        title: "",
-        description: "",
+        id: 3,
+        image: "/assets/how-it-works/1.jpg",
+        category: [1,3],
+        title: "how serving our communities in 2024 inspires our innovation",
+        description: "Our core values are Heart, Communication, Customer Centricity, Bias for Action, and Collaboration",
+        link: ""
     },
     {
-        image: "",
-        category: [],
-        title: "",
-        description: "",
+        id: 4,
+        image: "/assets/how-it-works/1.jpg",
+        category: [1,3],
+        title: "Alternative data is just one consideration for a lender’s credit data strategy",
+        description: "Many fintechs are promoting this add-on as a panacea for any trouble related to model",
+        link: ""
     },
     {
-        image: "",
-        category: [],
-        title: "",
-        description: "",
+        id: 5,
+        image: "/assets/how-it-works/1.jpg",
+        category: [1,3],
+        title: "how serving our communities in 2024 inspires our innovation",
+        description: "Our core values are Heart, Communication, Customer Centricity, Bias for Action, and Collaboration",
+        link: ""
+    },
+    {
+        id: 6,
+        image: "/assets/how-it-works/1.jpg",
+        category: [1,3],
+        title: "Learning from yesterday’s credit union movement for a better tomorrow",
+        description: "Every year, we celebrate International Credit Union (ICU) Day on the third Thursday of October.",
+        link: ""
+    },
+    {
+        id: 7,
+        image: "/assets/how-it-works/1.jpg",
+        category: [1,3],
+        title: "how serving our communities in 2024 inspires our innovation",
+        description: "Our core values are Heart, Communication, Customer Centricity, Bias for Action, and Collaboration",
+        link: ""
     },
 ]
 
+const recentBlog = [
+    1,3,5
+]
+
 const Hero2 = () => {
+    const [activeCategory, setActiveCategory] = useState("All Blogs & Articles")
+
     return (
-        <div>
+        <div className="pt-16">
             {/* Navigation Buttons */}
-            <div className="flex justify-center mb-8">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    All
-                </button>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Category 1
-                </button>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Category 2
-                </button>
+            <div className="flex container w-[80%] items-center justify-center mb-8 border-t border-b  p-4 gap-4">
+                <label className="uppercase font-bold">Topic: </label>
+                {CategoryList.map((category, index) => (
+                    <button onClick={()=>setActiveCategory(category)} className={`${category === activeCategory ? 'bg-gray-300 text-[#004F8F] font-bold' : ''}  text-sm py-2 px-4 rounded`}>
+                        {category}
+                    </button>
+                ))}
             </div>
 
             {/* Card Boxes */}
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 px-4 py-4 container w-[80%]">
                 {BlogList.map((blog, index) => (
-                    <div className="bg-white rounded shadow-md p-4">
-                        <Image src="/image2.jpg" alt="Image 2" width={300} height={200} />
-                        <button className="text-gray-600">{blog.category}</button>
-                        <h2 className="text-2xl font-bold mb-2">{blog.title}</h2>
-                        <p className="text-gray-600">{blog.description}</p>
-                        <Link href="/blog/post2" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            Read More
-                        </Link>
-                    </div>
+                    <Blog
+                        image={blog.image}
+                        category={blog.category}
+                        title={blog.title}
+                        description={blog.description}
+                        link={blog.link}
+                    />
                 ))}
             </div>
 
             <div className="items-center bg-gray-300 py-4">
                 <h1 className="text-[#004F8F] mx-auto text-4xl py-4 text-center w-1/3">Recently Uploaded <span className="text-black">Blogs and Articles</span></h1>
-                <div className="flex gap-2 px-[20%] py-4">
-                    {/* {BlogList.map((card, index) => (
-                        <Card key={index} title="" image={card.image} subtitle={card.subtitle} description={card.description} result={card.result} detail={card.detail} />
-                    ))} */}
+                <div className="grid grid-cols-1 md:grid-cols-5 gap-4 px-4">
+                    <div></div>
+                    {recentBlog.map((recent, index) => (
+                        BlogList.find(blog => blog.id === recent) && (
+                            <Blog
+                                key={index}
+                                image={BlogList.find(blog => blog.id === recent)?.image || ''}
+                                category={BlogList.find(blog => blog.id === recent)?.category || []}
+                                title={BlogList.find(blog => blog.id === recent)?.title || ''}
+                                description={BlogList.find(blog => blog.id === recent)?.description || ''}
+                                link={BlogList.find(blog => blog.id === recent)?.link || ''}
+                            />
+                        )
+                    ))}
+                    <div></div>
                 </div>
             </div>
         </div>
